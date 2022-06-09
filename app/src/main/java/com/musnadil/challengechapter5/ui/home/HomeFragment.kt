a3146643cd74112cdb9f01919ddea2fec7af991a
Musnadil <firdaus.musnadil@gmail.com>
2022-06-09 19:36:50 +0700

    test CI-6

--- app/src/main/java/com/musnadil/challengechapter5/ui/home/HomeFragment.kt
+++ app/src/main/java/com/musnadil/challengechapter5/ui/home/HomeFragment.kt
@@ -67,7 +67,7 @@ class HomeFragment : Fragment() {
             setCountry()
         }
     }
-    //test CI-5
+    //test CI-6
 
     private fun fatchNews(country: String) {
         val apiKey = "de0e45bbc3fd4286b6d2cf8120c756ea"
